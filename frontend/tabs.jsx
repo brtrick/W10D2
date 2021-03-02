4d8c23994a852ecd24e1e56b5a355804d0f9fe6a
Andrew Kim <andrewikim95@gmail.com>
2021-03-02 17:03:00 -0500

    finished tabs :D

--- frontend/tabs.jsx
+++ frontend/tabs.jsx
@@ -5,22 +5,27 @@ export default class Tabs extends React.Component {
         super(props);
         this.state = {
             index: 0,
+            prevIndex: 1,
         }
         this.tabClick = this.tabClick.bind(this);
     }
 
     tabClick(e) {
-
+        this.setState({prevIndex: this.state.index});
+        this.setState({index: e.currentTarget.getAttribute('data-key')});
     }
 
     render() {
+        
         return (
             <div id="tabs-div">
                 <ul id="tabs-header">
-                    {this.props.panes.map((tab, i) => <li key={i} onClick={tabClick}><h1>{tab.title}</h1></li>)}
+                    {this.props.panes.map((tab, i) => <li key={i} data-key={i} onClick={this.tabClick}><h1>{tab.title}</h1></li>)}
                 </ul>
                 <article>
-                    {this.props.panes.map((tab, i) => <p key={i}>{tab.content}</p>)}
+                    {<p className="hidden" key={this.state.prevIndex}>{this.props.panes[this.state.prevIndex].content}</p>}
+
+                    {<p className="appear" key={this.state.index}>{this.props.panes[this.state.index].content}</p>}
                 </article>
             </div>
         );

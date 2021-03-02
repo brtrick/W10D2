6a113faf97a31b366b8fae00cd7fc5ffc1b288f0
Andrew Kim <andrewikim95@gmail.com>
2021-03-02 14:58:23 -0500

    Finished our clock and style it too!

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -7,12 +7,34 @@ export default class Clock extends React.Component {
         this.state = {
             date: new Date(),
         }
+        this.intervalId = null;
+
+        this.tick = this.tick.bind(this);
+
     }
 
     render() {
         return (
-            <h1>The Time Is Now</h1>
+            <>
+                <h1 className="clock-header">The Time Is Now</h1>
+                <ul className="clock-widgit">
+                    <li className="clock-time">Time: <span>{this.state.date.toTimeString().slice(0,8)} EST</span></li>
+                    <li className="clock-date">Date: <span>{this.state.date.toDateString()}</span></li>
+                </ul>
+            </>
         );
+    }
+
+    tick() {
+        this.setState({date: new Date()});
+    }
+
+    componentDidMount(){
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount(){
+        cancelInterval(this.intervalId);
     }
 }
 
